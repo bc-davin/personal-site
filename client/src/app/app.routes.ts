e2aa95570300161e5a907103044c3240d24ce825
Davin <71451687+bc-davin@users.noreply.github.com>
2025-12-30 11:15:27 -0700

    save

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,13 +3,11 @@ import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { BlogComponent } from './blog/blog.component';
 import { BlogDetailComponent } from './blog/blog-detail.component';
-import { ExperienceComponent } from './experience/experience.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'blog', component: BlogComponent },
   { path: 'blog/:id', component: BlogDetailComponent },
-  { path: 'experience', component: ExperienceComponent },
   { path: '**', redirectTo: '' }
 ];
